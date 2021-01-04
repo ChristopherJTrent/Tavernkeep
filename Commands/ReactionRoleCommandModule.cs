952c9d18d8fcc4b94dfaaed52d6a0e93e6d23f36
ChristopherJTrent <TrentChristopher@student.deanza.edu>
2021-01-03 19:00:44 -0800

    Initial Commit, Halfway through the reaction roles command.

---
+++ Commands/ReactionRoleCommandModule.cs
@@ -0,0 +1,81 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Tavernkeep.Commands
+{
+	class ReactionRoleCommandModule : BaseCommandModule
+	{
+		private Dictionary<DiscordEmoji, DiscordRole> EmojiRolePairs = new Dictionary<DiscordEmoji, DiscordRole>();
+		[Command("reactionroles")]
+		public async Task SetupReactionRolesCommand(CommandContext ctx, DiscordMember invoker)
+		{
+			List<DiscordMessage> messagesForCleanup = new List<DiscordMessage>();
+			var channelTagMessage = await ctx.RespondAsync("Please tag the channel you wish to have the reaction roles message be in.");
+			messagesForCleanup.Add(channelTagMessage);
+
+			var response = await ctx.Message.GetNextMessageAsync();
+			while (response.TimedOut || response.Result.MentionedChannels.Count == 0) {
+				messagesForCleanup.Add(await ctx.RespondAsync("Please make sure to tag the channel you want place the reaction roles message in."));
+				response = await ctx.Message.GetNextMessageAsync();
+			}
+
+			messagesForCleanup.Add(response.Result);
+			var ReactionRoleMessageChannel = response.Result.MentionedChannels.First();
+
+			var builder = new DiscordEmbedBuilder()
+					.WithTitle("Reaction Roles Setup")
+					.WithColor(DiscordColor.DarkGreen)
+					.WithDescription($"Please React to this message with the emoji you want to use, then follow the prompts before chosing another.\n" +
+						$"React with {DiscordEmoji.FromName(ctx.Client, "stop_sign")} to finish.\n" +
+						$"Chosen Reaction Roles:\n")
+					.WithTimestamp(DateTime.Now);
+
+			var ReactionRoleReactionSelectionMessage = await ctx.RespondAsync(null, false, builder.Build());
+			messagesForCleanup.Add(ReactionRoleReactionSelectionMessage);
+			var result = await ReactionRoleReactionSelectionMessage.WaitForReactionAsync(invoker);
+
+			Dictionary<int, DiscordRole> OrderedRoles = new Dictionary<int, DiscordRole>();
+			StringBuilder RolesStringBuilder = new StringBuilder();
+			int lastRoleNumber = 1;
+
+			foreach (var role in ctx.Guild.Roles) {
+				RolesStringBuilder.Append($"`{lastRoleNumber}`: `{role.Value.Name}`\n");
+				OrderedRoles.Add(lastRoleNumber++, role.Value);
+			}
+			var RolesString = RolesStringBuilder.ToString();
+			while (!result.TimedOut) {
+				if (result.Result.Emoji.Equals(DiscordEmoji.FromName(ctx.Client, "stop_sign"))){
+					foreach (var m in messagesForCleanup) {
+						await m.DeleteAsync("Tavernkeep Interaction Cleanup");
+					}
+					return;
+				}
+				if (!EmojiRolePairs.ContainsKey(result.Result.Emoji)) {
+					var roleRequestEmbed = new DiscordEmbedBuilder()
+						.WithTitle($"Role to assign for {result.Result.Emoji}")
+						.WithColor(DiscordColor.DarkGreen)
+						.WithDescription("Please respond with a role number from the following list:\n" + RolesString)
+						.Build();
+					var roleSelectionMessage = await ctx.RespondAsync(null, false, roleRequestEmbed);
+					
+				}
+			}
+		}
+
+		private DiscordEmbed getUpdatedEmbedForReactionRoles(DiscordEmbed existing, DiscordEmoji emoji, DiscordRole role)
+		{
+			var builder = new DiscordEmbedBuilder(existing);
+			builder.WithDescription(existing.Description + $"\n{emoji}: `{role.Name}`");
+			return builder.Build();
+		}
+	}
+}
---
+++ Core.cs
@@ -0,0 +1,42 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Enums;
+using DSharpPlus.Interactivity.Extensions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Tavernkeep
+{
+	class Core
+	{
+		static void Main(string[] args)
+		{
+			mainAsync().GetAwaiter().GetResult();
+		}
+		public static async Task mainAsync()
+		{
+			using FileStream stream = File.OpenRead("./configuration.json");
+			Dictionary<string, string> Config = await JsonSerializer.DeserializeAsync<Dictionary<String, String>>(stream);
+			var discord = new DiscordClient(new DiscordConfiguration() {
+				Token = Config["AccessToken"],
+				TokenType = TokenType.Bot
+			});
+			var CommandsNextObj = discord.UseCommandsNext(new CommandsNextConfiguration() {
+				StringPrefixes = new[] {Config["CommandPrefix"]}
+			});
+
+			CommandsNextObj.RegisterCommands<Commands.ReactionRoleCommandModule>();
+
+			var InteractivityObj = discord.UseInteractivity(new InteractivityConfiguration() {
+				PollBehaviour = PollBehaviour.DeleteEmojis,
+				Timeout = TimeSpan.FromSeconds(60)
+			});
+			await discord.ConnectAsync();
+			await Task.Delay(-1);
+		}
+	}
+}
